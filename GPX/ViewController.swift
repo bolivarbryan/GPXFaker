1957fab33338d968c9d19daaad20e1901809605d
Bryan Andres Bolivar Martinez <bbolivar@belatrixsf.com>
2017-07-26 11:33:08 -0500

    Initial Commit

---
+++ GPX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPX
+//
+//  Created by Bryan Andres Bolivar Martinez on 7/26/17.
+//  Copyright © 2017 Bryan Andres Bolivar Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
